7e2763c9e52d4e977d11d5a341abdc2dd3e68f41
Торбунова Екатерина Александровна <etorbunova@tsc.ts>
2021-05-21 16:34:10 +0300

    Update Subtask.jsx

--- react/my-pain/test-app/src/Components/Subtask.jsx
+++ react/my-pain/test-app/src/Components/Subtask.jsx
@@ -34,7 +34,7 @@ function Subtask({subtask, task, deleteSubtask, checkboxSubtask, renameSubtask})
                 <input className={"input",{"inputLineThrough":subtask.completed}} type="text" value={editValue} disabled={subtask.completed || task.completed} onChange={e => setEditValue(e.target.value)}/>
             </div>
             <div className="ChangeButtons">
-                {subtask.completed && <button style={styles.button} disabled={subtask.completed || task.completed} onClick={() => checkboxSubtask(subtask) } >restore</button>}
+                {subtask.completed && <button style={styles.button} disabled={task.completed} onClick={() => checkboxSubtask(subtask) } >restore</button>}
                 {!subtask.completed && <button style={styles.button} disabled={subtask.completed || task.completed} onClick={() => {renameSubtask(subtask, editValue)}}>save changes</button>}
                 {!subtask.completed && <button style={styles.delete} disabled={subtask.completed || task.completed} onClick={() => deleteSubtask(subtask.id)}>delete</button>}
             </div>
